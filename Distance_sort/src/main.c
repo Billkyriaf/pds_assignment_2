26f4b5ec3540e3a7e05f260d1e397961b98f2c42
Billkyriaf <bkyriafinis@gmail.com>
2021-12-03 02:46:56 +0200

    Test main

--- Distance_sort/src/main.c
+++ Distance_sort/src/main.c
@@ -7,7 +7,6 @@
 
 
 int main(int argc, char **argv) {
-    printf("%s\n", argv[1]);
     // Initialize the MPI communication
     MPI_Init(&argc, &argv);
 
@@ -19,7 +18,6 @@ int main(int argc, char **argv) {
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-
     // Read the initial points
     Point *points = NULL;  // All the points for each process
     int pointsPerProcess;  // The total number of points each process has
@@ -29,34 +27,27 @@ int main(int argc, char **argv) {
     readFromFile(argv[1], points, world_rank, world_size, &pointsDimension, &pointsPerProcess);
 
 
-
     MPI_Request mpiRequest;
     MPI_Status mpiStatus;
 
     if (world_rank == 0) {
-        printf("Master thread sending...\n");
+        for (int i = 0; i < pointsPerProcess; ++i) {
+            printf("Rank: %d, Point %d coordinates: ",world_rank, i);
+            for (int j = 0; j < pointsDimension; ++j) {
+                printf("%f ", points[i].coordinates[j]);
+            }
 
-        int *arr;
-        arr = (int *)malloc(world_size * sizeof(int));
-
-        for (int i = 0; i < world_size; i++) {
-            arr[i] = i;
-            MPI_Isend((arr + i), 1, MPI_INT, i, 10, MPI_COMM_WORLD, &mpiRequest);
+            printf("\n");
         }
-
-        MPI_Wait(&mpiRequest, &mpiStatus);
-
-        free(arr);
-        printf("Finished\n");
-
     } else {
-        int a = 0;
+        for (int i = 0; i < pointsPerProcess; ++i) {
+            printf("Rank: %d, Point %d coordinates: ",world_rank, i);
+            for (int j = 0; j < pointsDimension; ++j) {
+                printf("%f ", points[i].coordinates[j]);
+            }
 
-        int receive[1];
-
-        MPI_Recv(receive, 1, MPI_INT, 0, 11, MPI_COMM_WORLD, &mpiStatus);
-
-        printf("Rank %d received: %d, a = %d\n", world_rank, receive[0], a);
+            printf("\n");
+        }
     }
 
     // Finalize the MPI environment.
