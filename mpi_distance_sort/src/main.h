d8e1d869ba8fc38287d172236fbb06cff53cedb6
Billkyriaf <bkyriafinis@gmail.com>
2021-12-22 03:51:56 +0200

    Project files update

--- mpi_distance_sort/src/main.h
+++ mpi_distance_sort/src/main.h
@@ -8,8 +8,9 @@
  */
 typedef struct info{
     int world_size;  // The size of the world (number of processes)
+    int initial_size;  // The size of the MPI_COMM_WORLD
     int world_rank;  // The rank of the current process
-    int initial_rank;
+    int initial_rank;  // The rank of the process in the MPI_COMM_WORLD
 
 
     double median;  // The median value used for testing
